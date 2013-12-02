d7e1486dc4b5962c9234a539e055c86b1759ab62
Richard McCartney <richard@message.co.uk>
2013-12-02 11:49:51 +0000

    Start of the html boilerplate files

---
+++ HTML Boilerplate/js/setup.js
@@ -0,0 +1,14 @@
+/**
+ * Boiler plate jQuery
+ *
+ * All general site, setup and plugin scripts are to be contained here
+ *
+ * @author Richard McCartney <richard@message.co.uk>
+ */
+
+jQuery(document).ready(function($) {
+
+	// Set JS class
+	$('.no-js').removeClass('no-js').addClass('js');
+
+});
